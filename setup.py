2da0b8a324a3806f3a158d96456879d658a8768e
noklam <channoklam@yahoo.com.hk>
2020-11-10 14:17:27 +0800

    add setup.py for installing the package locally

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='dtype_diet',
+   version='0.0.1',
+   description='A useful module',
+   author='Ian Ozsvald',
+   author_email='foomail@foo.com',
+   packages=[],  #same as name
+   install_requires=[], #external packages as dependencies
+)
